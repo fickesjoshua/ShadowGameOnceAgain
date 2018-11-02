90d47761202fc5b345f16874f4f5999397297108
fickesjoshua <fickes@usc.edu>
2018-11-02 01:05:23 -0700

    Backup
    Here it is.

---
+++ ShadowUnityGameBackup/Assets/Code/Hazardous.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Hazardous : MonoBehaviour {
+
+    bool dead = false;
+    public int xRespawn = 0;
+    public int yRespawn = 0;
+
+    // Use this for initialization
+    void Start () {
+	}
+
+    //   void OnControllerColliderHit(ControllerColliderHit hit)
+    //    {
+
+    //        if (hit.gameObject.tag == "hazard")
+    //      {
+    //         dead = true;
+    //        Debug.Log("found a death thing");
+    //      }
+    //              }
+    //
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+            if (collision.gameObject.tag == "hazard")
+            {
+                dead = true;
+            Debug.Log("found a dead thing");
+            }
+        }    
+
+    // Update is called once per frame
+    void Update () {
+
+        if (dead)
+        {
+            Debug.Log("omg im literally dying");
+            Vector2 v2 = new Vector2(xRespawn, yRespawn);
+            transform.position = v2;
+            dead = false;
+        }
+	}
+}
---
+++ ShadowUnityGameBackup/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+
+	bool jump = false;
+
+    Animator animator; //Get the animator connected to the player.
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw ("Horizontal") * runSpeed;
+
+        if (horizontalMove == 0) //Check if player isstanding still or moving and changes animation.
+        {
+            animator.SetBool("IsRunning", false);
+        } else
+        {
+            animator.SetBool("IsRunning", true);
+        }
+
+		if (Input.GetButtonDown ("Jump")) {
+			jump = true;
+            animator.SetTrigger("Jumps"); //Makes jump animation play.
+        }
+
+	}
+
+	void FixedUpdate () {
+
+		controller.Move (horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+}
